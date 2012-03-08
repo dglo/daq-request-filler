fdfc08ae536ac5b0683ea74766e16bf983eecc87
Dave Glowacki <dglo@icecube.wisc.edu>
2012-03-08 10:13:47 -0600

    Issue 5302: Add new files which I missed in the last commit

---
+++ src/main/java/icecube/daq/reqFiller/DummyPayload.java
@@ -0,0 +1,91 @@
+package icecube.daq.reqFiller;
+
+import icecube.daq.payload.IByteBufferCache;
+import icecube.daq.payload.ILoadablePayload;
+import icecube.daq.payload.IPayload;
+import icecube.daq.payload.IUTCTime;
+import icecube.daq.splicer.Spliceable;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.zip.DataFormatException;
+
+public class DummyPayload
+    implements ILoadablePayload
+{
+    public DummyPayload() {
+    }
+
+    /**
+     * This method allows a deepCopy of itself.
+     *
+     * @return Object which is a copy of the object which implements this interface.
+     */
+    public Object deepCopy() {
+        return new DummyPayload();
+    }
+
+    public ByteBuffer getPayloadBacking()
+    {
+        throw new Error("Unimplemented");
+    }
+
+    /**
+     * returns the Payload interface type as defined in the PayloadInterfaceRegistry.
+     *
+     * @return int ... one of the defined types in icecube.daq.payload.PayloadInterfaceRegistry
+     */
+    public int getPayloadInterfaceType() {
+        return -1;
+    }
+
+    /**
+     * returns the length in bytes of this payload
+     */
+    public int getPayloadLength() {
+        return 0;
+    }
+
+    /**
+     * gets the UTC time tag of a payload
+     */
+    public IUTCTime getPayloadTimeUTC() {
+        return null;
+    }
+
+    /**
+     * returns the Payload type
+     */
+    public int getPayloadType() {
+        return -1;
+    }
+
+    /**
+     * gets the UTC time tag of a payload as a long value
+     */
+    public long getUTCTime()
+    {
+        return 0;
+    }
+
+    public void loadPayload()
+        throws IOException, DataFormatException
+    {
+        // do nothing
+    }
+
+    public void recycle()
+    {
+        // do nothing
+    }
+
+    public void setCache(IByteBufferCache cache)
+    {
+        // do nothing
+    }
+
+    public String toString()
+    {
+        return "Dummy";
+    }
+}
